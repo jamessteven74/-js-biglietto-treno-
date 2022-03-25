6122c8648a6b7bb55051b7812ddb006728de6913
jamessteven74 <jamesstevenvictorien@gmail.com>
2022-03-25 15:07:09 +0100

    add_price_for user

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,21 +8,52 @@ va applicato uno sconto del 40% per gli over 65.
 */
 
 //chiedere all'utente il numero  di chilometri che vuole percorrere
-const numbers_km = prompt("quanti chilometri devi percorrere?");
+
+
+const numbers_km = parseInt(prompt("quanti chilometri devi percorrere?"));
 console.log(numbers_km)
 //50 km
+document.getElementById('numbers_km').innerHTML = numbers_km;
 
-document.getElementById('numbers_km').innerHTML = numbers_km
 
 //età del passeggero
-const user_age = prompt ("quanti anni hai?");
+const user_age = parseInt(prompt ("quanti anni hai?"));
 console.log(user_age)
 //70 anni
+document.getElementById('user_age').innerHTML = user_age;
 
-document.getElementById('user_age').innerHTML = user_age
 
-// prezzo del biglietto
-let x = 70;
-let Y = 0.21;
-let z = x*z;
+//costo del biglietto
+const price_km = 0.21;
+console.log(price_km)
+
+const prezzo_standard = numbers_km * price_km;
+console.log(prezzo_standard)
+
+//prezzo per over 65
+const prezzo_over_65 = prezzo_standard - (prezzo_standard * 0,4)
+console.log(prezzo_over_65)
+
+//prezzo per minorenne
+const prezzo_minorenne = prezzo_standard - (prezzo_standard * 0.2)
+console.log(prezzo_minorenne)
+
+if (user_age < 18){
+    prezzo_minorenne
+
+} else if (user_age > 65){
+    prezzo_over_65
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
